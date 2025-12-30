91a4f3dcdb3fab6331489058f74dc056c567eb87
Ori Elbaz <83533131+OriElbaz@users.noreply.github.com>
2025-12-30 23:44:26 +1100

    feat: astroids spawn in game

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -10,16 +12,25 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    Player.container = (updatable, drawable)
+    
+    
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
 
     clock = pygame.time.Clock()
     dt = 0
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-    updatable.add(player)
-    drawable.add(player)
+
+    Player(x, y)
+    AsteroidField()
+
 
     while True:
         log_state()
